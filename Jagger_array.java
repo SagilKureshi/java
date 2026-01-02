e4d2550b5cee64a4d1691bafe49b03e5648c13ad
Sagil-Kureshi <sagilkureshi007@gmail.com>
2026-01-02 11:46:24 +0530

    Add files via upload

---
+++ Jagger_array.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+class Jagger_array {
+    public static void main(String[] args) {
+
+        int[][] SK = new int[3][];
+
+        SK[0] = new int[3];
+        SK[0][0] = 11;
+
+        SK[1] = new int[2];
+        SK[1][0] = 23;
+
+        SK[2] = new int[2];
+        SK[2][0] = 89;
+
+        System.out.println(Arrays.deepToString(SK));
+    }
+}
