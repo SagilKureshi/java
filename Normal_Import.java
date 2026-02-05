e8c5372f9c7b5256e37c7a45bfdecca3c4e726a5
Sagil-Kureshi <sagilkureshi007@gmail.com>
2026-02-05 09:44:56 +0530

    Add files via upload

---
+++ Normal_Import.java
@@ -0,0 +1,8 @@
+import java.lang.*;
+class Normal_Import{
+	public static void main(String SK[]){
+		System.out.println("Square Root of 16 = "+Math.sqrt(16));
+		System.out.println("2 power of 5 = "+Math.pow(5,2));
+		System.out.println("Absolute value of -18 = "+Math.abs(-18));
+	}
+}
---
+++ Static_Import.java
@@ -0,0 +1,9 @@
+import static java.lang.Math.*;
+class Static_Import{
+	public static void main(String SK[]){
+		System.out.println("Square Root of 25 = "+sqrt(25));
+		System.out.println("3 power of 5 = "+pow(5,3));
+		System.out.println("Absolute value of -18 = "+abs(-18));
+		
+	}
+}
