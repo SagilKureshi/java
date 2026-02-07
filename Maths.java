3678a2891ede34cc14ff01802b701413f28dc3de
Sagil-Kureshi <sagilkureshi007@gmail.com>
2026-02-07 08:47:38 +0530

    Add files via upload

---
+++ Maths.java
@@ -0,0 +1,32 @@
+import java.lang.*;
+class Maths{
+	public static void main(String SK[]){
+		int s = 18,k = -7;
+		double f = 5.5, t = 7.7;
+		System.out.println("Value Integer of S = "+s);
+		System.out.println("Value Integer of K = "+k);
+		System.out.println("Value Double of F = "+f);
+		System.out.println("Value Double of T = "+t);
+		
+		System.out.println("Absolute value of S = "+Math.abs(s));
+		System.out.println("Absolute value of K = "+Math.abs(k));
+		System.out.println("Absolute value of F = "+Math.abs(f));
+		System.out.println("Absolute value of T = "+Math.abs(t));
+
+		System.out.println("Round value of S = "+Math.abs(s));
+		System.out.println("Round value of K = "+Math.abs(k));
+		System.out.println("Round value of F = "+Math.abs(f));
+		System.out.println("Round value of T = "+Math.abs(t));
+		
+		System.out.println("Minimum of (S,K) = "+Math.min(s,k));
+		System.out.println("Minimum of (F,T) = "+Math.min(f,t));
+		
+		System.out.println("Maximum of (S,K) = "+Math.max(s,k));
+		System.out.println("Maximum of (F,T) = "+Math.max(f,t));
+		
+		System.out.println("Square Root of S = "+Math.sqrt(s));
+		System.out.println("Square Root of K = "+Math.sqrt(k));          M
+		System.out.println("Square Root of F = "+Math.sqrt(f));
+		System.out.println("Square Root of T = "+Math.sqrt(t));
+	}
+}
